c01eca07f397c53bbc7ee36d81bed9ea7a2f0831
Young Joon Lee <yj.lee@chu.ac.kr>
2022-12-07 18:37:59 +0900

    added config, install, git, ssh, github, zsh scripts

--- run_once_install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-{{ if eq .chezmoi.os "linux" -}}
-sudo apt install zsh
-sudo chsh -s $(which zsh)
-{{ else if eq .chezmoi.os "darwin" -}}
-brew install zsh
-chsh -s $(which zsh)
-{{ end -}}
